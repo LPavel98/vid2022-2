81e6bfbe551b510dede0a6db93fc2e3fdb1cab0f
LPavel98 <pavel.perez.alvarez@gmail.com>
2022-08-24 11:25:48 -0500

    primer commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float JumpForce = 5;
+
+    public float velocity = 10;
+    Rigidbody2D rb; 
+    SpriteRenderer sr;
+    Animator animator;
+    const int ANIMATION_QUIETO = 0;
+    const int ANIMATION_CORRER = 1;
+
+    bool puedeSaltar = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Iniciamos script de player");
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.velocity.x;
+        //rb.velocity.y;
+
+        //GetKey
+        //GetKeyUp
+        //GetKeyDown
+
+        
+        
+        Debug.Log("Puede saltar"+puedeSaltar.ToString());
+        if (Input.GetKey(KeyCode.RightArrow)){
+            rb.velocity = new Vector2(velocity, rb.velocity.y);  
+            sr.flipX = false;
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)){
+            rb.velocity = new Vector2(-velocity, rb.velocity.y);
+            sr.flipX = true;
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Space) && puedeSaltar)
+        {
+            rb.AddForce(new Vector2(0,JumpForce), ForceMode2D.Impulse);
+            puedeSaltar = false;
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            ChangeAnimation(ANIMATION_QUIETO);
+        }
+            
+
+    }  
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Puede saltar");
+        puedeSaltar = true;
+            if (other.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Estas muerto");
+            }   
+    }
+   
+
+    private void ChangeAnimation(int animation){
+        animator.SetInteger("Estado", animation);
+
+    }
+
+}
